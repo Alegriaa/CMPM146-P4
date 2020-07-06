4e7f27a6498dadebc104ed47aeb5f0ab36f57d9a
Brian <brpereir@ucsc.edu>
2020-07-05 22:31:02 -0700

    beats all bots

--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -5,6 +5,7 @@ each  function  should  only  take  the  game
 state  as  a  parameter. 
 """
 
+
 def if_neutral_planet_available(state):
     return any(state.neutral_planets())
 
@@ -14,3 +15,4 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
